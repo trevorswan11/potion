50975388111162141fd40149b6d7c6fd3c74833a
trevorswan11 <tcs94@case.edu>
2026-01-31 22:50:46 -0500

    analog writing

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,36 @@
 const std = @import("std");
 const microzig = @import("microzig");
+
 const rp2xxx = microzig.hal;
 const time = rp2xxx.time;
 const gpio = rp2xxx.gpio;
+const pwm = microzig.chip.peripherals.PWM;
 
-const pin_config = rp2xxx.pins.GlobalConfiguration{
-    .GPIO25 = .{
+const pin_config: rp2xxx.pins.GlobalConfiguration = .{
+    .GPIO15 = .{
         .name = "led",
         .direction = .out,
+        .function = .PWM7_B,
     },
 };
+const pins = pin_config.pins();
+
+const LOW: u1 = 0;
+const HIGH: u1 = 1;
 
 pub fn main() !void {
     pin_config.apply();
-    const pin = gpio.num(25);
-    
+    pins.led.slice().set_wrap(100);
+    pins.led.slice().enable();
+
     while (true) {
-        pin.toggle();
-        time.sleep_ms(500);
+        for (0..101) |level| {
+            pins.led.set_level(@truncate(level));
+            time.sleep_ms(10);
+        }
+        for (1..100) |level| {
+            pins.led.set_level(@truncate(100 - level));
+            time.sleep_ms(10);
+        }
     }
 }
