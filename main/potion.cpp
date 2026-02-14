96c97ab360ef8eb2badc739a6c94aed78ac84452
trevorswan11 <tcs94@case.edu>
2026-02-14 11:11:05 -0500

    So glad this works, going to move this into micro for example sake

--- main/potion.cpp
+++ main/potion.cpp
@@ -4,17 +4,18 @@
 #include "gpio_cxx.hpp"
 
 using namespace idf;
+using namespace std::chrono_literals;
 
 extern "C" auto app_main() -> void {
-    const GPIO_Output gpio{GPIONum(13)};
+    const GPIO_Output led{GPIONum(13)};
 
     while (true) {
         std::println("LED ON");
-        gpio.set_high();
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        led.set_high();
+        std::this_thread::sleep_for(1s);
 
         std::println("LED OFF");
-        gpio.set_low();
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        led.set_low();
+        std::this_thread::sleep_for(1s);
     }
 }
